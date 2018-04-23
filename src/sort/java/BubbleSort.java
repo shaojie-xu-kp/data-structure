9e1eaa196d1b8b3be11117a5e92710a05563089a
shaojie.xu <xsj8585@hotmail.com>
2018-04-23 11:51:56 +0100

    add java bubble sort

---
+++ src/sort/java/BubbleSort.java
@@ -0,0 +1,28 @@
+package sort.java;
+
+import java.util.Arrays;
+
+/**
+ * a simple java imperative way of implementing bubble sort
+ */
+public class BubbleSort {
+
+    public static void main(String... args) {
+        int[] a = {7,8,9,1,2,3,10,23,0};
+        System.out.println(Arrays.toString(bubbleSort(a)));
+    }
+
+    private static int[] bubbleSort(int[] a) {
+        for(int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length -1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+}

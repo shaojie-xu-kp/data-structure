af06d9f9ce276d392d7397e1df389a0c9342b5d3
shaojie-xu-kp <xsj8585@hotmail.com>
2018-04-24 22:40:35 +0100

    complete scala version of binary search

--- src/search/scala/BinarySearch.scala
+++ src/search/scala/BinarySearch.scala
@@ -1,18 +1,32 @@
 package search.scala
 
-import sort.scala.{BubbleSort, HeapSort}
-
+import sort.scala.{HeapSort}
+import scala.annotation.tailrec
 object BinarySearch extends App {
 
-  def binarySearch(arr: Array[Int], i: Int, i1: Int, key: Int): Int = {
-    -1
-  }
 
   def search(arr: Array[Int], key : Int) : Int = {
-    binarySearch(arr, 0, arr.length - 1, key)
+    @tailrec
+    def binarySearch(minIndex: Int, maxIndex: Int): Int = {
+      val midIndex = (minIndex + maxIndex) / 2
+      if (midIndex > maxIndex)
+        -1
+      else if (arr(midIndex) == key)
+        midIndex
+      else if (arr(midIndex) > key)
+        binarySearch(minIndex, midIndex -1)
+      else
+        binarySearch(midIndex + 1, maxIndex)
+    }
+
+    binarySearch(0, arr.length - 1)
   }
   val array : Array[Int] = Array(1,3,5,2,6,4,9,0,30,12)
   HeapSort.sort(array)
   array foreach println
-
+  val index = search(arr = array, key = 0)
+  index match {
+    case -1 => println("key not found")
+    case _ => println(s"key found at position $index")
+  }
 }

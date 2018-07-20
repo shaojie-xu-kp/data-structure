1fed955d9226078c97923187d33e829e62bc6789
shaojie.xu <xsj8585@hotmail.com>
2018-07-20 09:16:53 +0100

    refactoring

--- src/main/java/sort/java/HeapSort.java
+++ src/main/java/sort/java/HeapSort.java
@@ -57,9 +57,7 @@ public class HeapSort {
     public static void main(String args[])
     {
         int arr[] = {12, 11, 13, 5, 6, 7};
-        int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+        sort(arr);
         Arrays.stream(arr).forEach(System.out::println);
     }
 }
